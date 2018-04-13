6c93c293a9e0e45b27346bd037be36fccb905157
Tejas Sharma <tejas.s1996@gmail.com>
2018-04-13 14:12:08 -0500

    fixed review text

--- pitchfork.py
+++ pitchfork.py
@@ -25,9 +25,7 @@ else:
 
 
 def replace_breaks(html):
-    """
-    Replaces all the <br> tags in the html with newlines '\n'
-    """
+    """Replace all the <br> tags in the html with newlines."""
     breakline = html.br
     while html.br is not None:
         breakline.insert_before('\n')
@@ -39,6 +37,7 @@ def replace_breaks(html):
 class Review:
     """
     Class representing the fetched review.
+
     Includes methods for getting the score, the text of the review
     (editorial), the album cover, label, year as well as the true
     (matched) album and artist names.
@@ -46,6 +45,7 @@ class Review:
 
     def __init__(self, searched_artist, searched_album, matched_artist,
                  matched_album, query, url, soup):
+        """Create a review object."""
         self.searched_artist = searched_artist
         self.searched_album = searched_album
         self.matched_artist = matched_artist
@@ -55,76 +55,89 @@ class Review:
         self.soup = soup
 
     def score(self):
-        """ Returns the album score. """
+        """Return the album score."""
         rating = self.soup.find(class_='score').text
         rating = float(rating.strip())
         return rating
 
+    def abstract(self):
+        """Return the text of the abstract."""
+        return self.soup.find(class_='review-detail__abstract').get_text()
+
     def editorial(self):
-        """ Returns the text of the review. """
-        return self.soup.find(class_='review-text').get_text()
+        """Return the main review text."""
+        return self.soup.find(class_='contents dropcap').get_text()
+
+    def full_text(self):
+        """Return a combination of the abstract and editorial."""
+        return "{}{}".format(self.abstract(), self.editorial())
 
     def cover(self):
-        """ Returns the link to the album cover. """
+        """Return the link to the album cover."""
         artwork = self.soup.find(class_='album-art')
         image_link = artwork.img['src'].strip()
         return image_link
 
     def artist(self):
-        """ Returns the artist name that pitchfork matched to our search. """
+        """Return the artist name that pitchfork matched to our search."""
         artist = self.matched_artist.strip()
         return artist
 
     def album(self):
-        """ Returns the album name that pitchfork matched to our search. """
+        """Return the album name that pitchfork matched to our search."""
         album = self.matched_album.strip()
         return album
 
     def label(self):
-        """ Returns the name of the record label that released the album. """
+        """Return the name of the record label that released the album."""
         labels = list(set(self.soup.findAll(class_='labels-list__item')))
         label = ' / '.join([l.get_text() for l in labels])
         return label
 
     def year(self):
         """
-        Returns the year the album was released.
+        Return the year the album was released.
+
         In case of a reissue album, the year of original release as well as
         the year of the reissue is given separated by '/'.
         """
-        year = str(self.soup.find(class_='year').contents[-1])
+        year = self.soup.find(class_='pub-date').get_text()
         return year
 
     def _json_safe_dict(self):
-        """
-        Returns a dictionary representation of object where
-        the soup key's value's special characters are escaped
-        """
+        """Return a dictionary representation of object where the soup key's value's special characters are escaped."""
         d = self.__dict__.copy()
         d['soup'] = d['soup'].prettify()
         return d
 
     def to_json(self):
-        """
-        Returns the attributes of the album review formatted as json.
-        """
+        """Return the attributes of the album review formatted as json."""
         d = self._json_safe_dict()
         return json.dumps(d)
 
     def __repr__(self):
-        return self.__class__.__name__+repr((self.searched_artist,
-                                             self.searched_album,
-                                             self.matched_artist,
-                                             self.matched_album,
-                                             self.query,
-                                             self.url,
-                                             str(self.soup.__class__)))
+        """Return a representation of the object."""
+        return self.__class__.__name__ + repr((self.searched_artist,
+                                               self.searched_album,
+                                               self.matched_artist,
+                                               self.matched_album,
+                                               self.query,
+                                               self.url,
+                                               str(self.soup.__class__)))
 
 
 class MultiReview(Review):
+    """
+    Class representing the fetched review.
+
+    Includes methods for getting the score, the text of the review
+    (editorial), the album cover, label, year as well as the true
+    (matched) album and artist names.
+    """
 
     def __init__(self, searched_artist, searched_album, matched_artist,
                  matched_album, query, url, soup):
+        """Create a review object."""
         self.searched_artist = searched_artist
         self.searched_album = searched_album
         self.matched_artist = matched_artist
@@ -135,60 +148,60 @@ class MultiReview(Review):
         self.info = soup.find('h2', text=self.matched_album).parent
 
     def score(self):
-        """ Returns the album score. """
+        """Return the album score."""
         rating = self.info.find(class_='score').text
         rating = float(rating.strip())
         return rating
 
     def label(self):
-        """ Returns the name of the record label that released the album. """
+        """Return the name of the record label that released the album."""
         label = self.info.h3.get_text()
         label = label[:label.index(';')].strip()
         return label
 
     def cover(self):
-        """ Returns the link to the album cover. """
+        """Return the link to the album cover."""
         artwork = self.info.parent.find(class_='artwork')
         image_link = artwork.img['src']
         return image_link
 
     def year(self):
         """
-        Returns the year the album was released.
+        Return the year the album was released.
+
         In case of a reissue album, the year of original release as well as
         the year of the reissue is given separated by '/'.
         """
         year = self.info.h3.get_text()
-        year = year[year.index(';')+1:].strip()
+        year = year[year.index(';') + 1:].strip()
         return year
 
     def _json_safe_dict(self):
-        """
-        Returns a dictionary representation of object where
-        the soup key's value's special characters are escaped
-        """
+        """Return a dictionary representation of object where the soup key's value's special characters are escaped."""
         d = self.__dict__.copy()
         d['soup'] = d['soup'].prettify()
         d['info'] = d['info'].prettify()
         return d
 
     def to_json(self):
+        """Return the attributes of the album review formatted as json."""
         d = self._json_safe_dict()
         return json.dumps(d)
+
 
 def search(artist, album):
     """
     Look for the review of the specified album by the specified artist.
+
     Returns either a Review object or a MultiReview object depending on
     the type of review because some pitchfork reviews cover multiple albums.
     """
-
     # replace spaces in the url with the '%20'
     query = re.sub('\s+', '%20', artist + '%20' + album)
     # using a custom user agent header
     request = Request(url='http://pitchfork.com/search/?query=' + query,
                       data=None,
-                      headers={'User-Agent': 'michalczaplinski/pitchfork-v0.1'})
+                      headers={'User-Agent': 'tejassharma/pitchfork-v0.1'})
     response = urlopen(request)
     text = response.read().decode('UTF-8').split('window.App=')[1].split(';</script>')[0]
 
@@ -208,7 +221,7 @@ def search(artist, album):
     full_url = urljoin('http://pitchfork.com/', url)
     request = Request(url=full_url,
                       data=None,
-                      headers={'User-Agent': 'michalczaplinski/pitchfork-v0.1'})
+                      headers={'User-Agent': 'tejassharma/pitchfork-v0.1'})
     response_text = urlopen(request).read()
     soup = BeautifulSoup(response_text, "lxml")
 
